521a7d50d88d37a5e5f50947cd7caebba36ed537
tlal98197-crypto <tlal98197@gmail.com>
2026-05-03 03:24:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-bot.send_photo(message.chat.id,
-    photo="https://i.ibb.co/tpfCthtk/image.jpg",
-    caption="""
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot("8701261854:AAGk_EGJTpKctYPaluNSO6QxE76TLogadR8")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton("➕ أضفني للمجموعة", url="https://t.me/axeror_bot?startgroup=true")
+    btn2 = types.InlineKeyboardButton("👨‍💻 المطور", url="https://t.me/ao1x1")
+    markup.add(btn1, btn2)
+    bot.send_photo(message.chat.id,
+        photo="https://i.ibb.co/tpfCthtk/image.jpg",
+        caption="""
 • أهلاً بك عزيزي 👋
 • أنا بوت اسمي يوهان 𒀭
 • اختصاصي حماية المجموعات 🛡
 • أضفني لمجموعتك وارفعني أدمن!
 • المطور ↤ @ao1x1
 """, reply_markup=markup)
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.reply_to(message, """
+📋 قائمة الأوامر:
+
+/start - بدء البوت 🤖
+/help - قائمة الأوامر 📋
+""")
+
+bot.infinity_polling()
